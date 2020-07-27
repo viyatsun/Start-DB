9c97b969e0115f3f5ec7be0db31016435d383e3d
Ksyunomorph <49567382+viyatsun@users.noreply.github.com>
2020-07-27 10:45:03 -0200

    fetch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-  return (
-    <div>StarDb</div>
-  );
+const getResource =  async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}` + 
+      `, recieved ${res.status}` )
+  }
+  const body = await res.json();
+  return body
 };
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+getResource ('https://swapi.dev/api/people/12132321321/')
+  .then((body)=>{
+    console.log(body)
+  })
+  .catch((err)=>{
+    console.error('Could no fetch', err)
+  })
+
+// fetch('https://swapi.dev/api/people/1/')
+//   .then((res)=>{
+//     return res.json()
+//   })
+//   .then((body)=>{
+//     console.log(body)
+//   });
+
