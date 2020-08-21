dd460b6aa0b55352d18a8cce5440c2b2467d7d5b
Ksyunomorph <49567382+viyatsun@users.noreply.github.com>
2020-08-21 16:16:23 +0300

    'cloning'

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 
 import Header from '../header';
+import ItemList from '../item-list';
 import './app.css';
 import ErrorIndicator from '../error-indicator';
 import SwapiService from '../../services/swapi-service';
 import ErrorBounry from '../error-boundry';
 import Row from '../row';
 import ItemDetails from '../item-details';
+import Record from '../record';
 
 
 export default class App extends Component {
@@ -32,20 +34,27 @@ export default class App extends Component {
         const{getPerson, 
               getStarship,
               getPersonImage,
-              getStarshipImage} = this.swapiService;
+              getStarshipImage,
+              getAllPlanets,
+              getAllPeople} = this.swapiService;
               
         const personDetails = (
             <ItemDetails itemId={5}
             getData= {getPerson}
-            getImageUrl ={getPersonImage}
-            />
+            getImageUrl ={getPersonImage}>
+                <Record field="gender" label ="Gender"/>
+                <Record field="eyeColor" label ="EyeColor"/>
+            </ItemDetails>
         );
 
         const starshipDetails = (
             <ItemDetails itemId={5}
             getData= {getStarship}
-            getImageUrl={getStarshipImage}
-            />
+            getImageUrl={getStarshipImage}>
+                <Record field="model" label ="Model"/>
+                <Record field="length" label ="Length"/>
+                <Record field="costInCredits" label ="Cost"/>
+            </ItemDetails>
         );
 
         return(
@@ -55,6 +64,16 @@ export default class App extends Component {
                 <Row left={personDetails} 
                     right= {starshipDetails}/>
             </div>
+            <ItemList 
+                getData={getAllPlanets}
+                onItemSelected={()=>{}}>
+                    {({name})=><span>{name}</span>}
+            </ItemList>
+            <ItemList 
+                getData={getAllPeople}
+                onItemSelected={()=>{}}>
+                    {({name})=><span>{name}</span>}
+            </ItemList>
         </ErrorBounry>
     )}
 };
--- src/components/item-details/item-details.js
+++ src/components/item-details/item-details.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import './item-details.css';
 import SwapiService from '../../services/swapi-service';
 
+
+
 export default class ItemDetails extends Component {
   
   swapiService = new SwapiService();  
@@ -40,6 +42,7 @@ export default class ItemDetails extends Component {
   
   
   render() {
+    const {item, image} = this.state
     if(!this.state.item) {
       return <span>Select a item from a list</span>;
     }
@@ -51,24 +54,17 @@ export default class ItemDetails extends Component {
     return (
       <div className="person-details card">
         <img className="person-image"
-          src={this.state.image} 
+          src={image} 
           />
 
         <div className="card-body">
             <h4>{name}</h4>
           <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Gender</span>
-              <span>{gender}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Birth Year</span>
-              <span>{birthYear}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Eye Color</span>
-              <span>{eyeColor}</span>
-            </li>
+           {
+             React.Children.map(this.props.children, (child)=>{
+              return React.cloneElement(child, {item}) ;
+             })
+           }
           </ul>
         </div>
       </div>
